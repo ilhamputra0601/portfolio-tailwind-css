e142a8e79e7151d8fb9afb6a8e16b5e66ed42430
Ilham Ramadhan <ilhamputra0601@gmail.com>
2023-01-03 15:20:00 +0700

    navbar jq

--- public/js/script.js
+++ public/js/script.js
@@ -1,10 +1,25 @@
 $(document).ready(function () {
+
+// Navbar Fixed
+const header = $('header');
+$(window).scroll(function() {    
+    let scroll = $(window).scrollTop();
+    if (scroll >= 1) {
+        $(header).addClass("navbar-fixed");
+    } else {
+        $(header).removeClass("navbar-fixed");
+    }
+});
+
 // Hamburger
-  const hamburger = $('#hamburger');
+const hamburger = $('#hamburger');
+const navMenu = $('#nav-menu') 
   $(hamburger).click(function (e) {
     $(hamburger).toggleClass("hamburger-active");
+    $(navMenu).toggleClass("hidden");
     e.preventDefault();
   });
+
 // darkmode
 const gelap = $("#darkmode")
 $(gelap).click(function (e) { 
