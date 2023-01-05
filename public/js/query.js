29a28fc9e92b5e816a02f0959d08b2443f2c8525
Ilham Ramadhan <ilhamputra0601@gmail.com>
2023-01-05 23:12:45 +0700

    fixed

---
+++ public/js/query.js
@@ -0,0 +1,54 @@
+$(document).ready(function () {
+  // scroll event
+  const header = $('header');
+  const top = $('#toTop');
+  $(window).scroll(function () {
+    let scroll = $(window).scrollTop();
+    // navbar fixed
+    if (scroll >= 1) {
+      $(header).addClass('navbar-fixed');
+    } else {
+      $(header).removeClass('navbar-fixed');
+    }
+    // scroll ToTop
+    if (scroll >= 1500) {
+      $(top).removeClass('hidding');
+    } else {
+      $(top).addClass('hidding');
+    }
+  });
+
+  // Hamburger
+  const hamburger = $('#hamburger');
+  const navMenu = $('#nav-menu');
+  $(hamburger).click(function (e) {
+    $(hamburger).toggleClass('hamburger-active');
+    $(navMenu).slideToggle('slow');
+    e.preventDefault();
+  });
+  // Hamburger close offside belum di buat
+
+  // darkmode
+  const dark = $('#dark');
+  $(dark).click(function (e) {
+    $('#terang').toggleClass('hidden');
+    $('#gelap').toggleClass('hidden');
+    let malam = $('#gelap').hasClass('hidden');
+    if (malam) {
+      $('html').addClass('dark');
+      localStorage.theme = 'dark';
+    } else {
+      $('html').removeClass('dark');
+      localStorage.theme = 'light'
+    }
+    if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+      $(gelap).hasClass('hidden') = true;
+    } else {
+      $(gelap).hasClass() = false;
+    }
+    e.preventDefault();
+  });
+
+
+  //pembungkus
+});
